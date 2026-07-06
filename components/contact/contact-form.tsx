74fd9a586f7af480e64972fc6ceb4790e97bbde1
Vishal Manam <vmanam1@asu.edu>
2026-07-05 18:59:45 -0700

    feat(contact): connect contact form to Formspree endpoint

--- components/contact/contact-form.tsx
+++ components/contact/contact-form.tsx
@@ -20,7 +20,7 @@ export function ContactForm() {
     const data = new FormData(form);
 
     try {
-      const res = await fetch("https://formspree.io/f/placeholder", {
+      const res = await fetch("https://formspree.io/f/mnjkwwlz", {
         method: "POST",
         body: data,
         headers: { Accept: "application/json" },
